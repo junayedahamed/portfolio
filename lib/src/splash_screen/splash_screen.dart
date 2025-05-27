0c647e0296573c60ad0502001398811a8a58ed21
junayedahamed <junayedahamed660@gmail.com>
2025-05-27 20:02:53 +0600

    splash_page_done

--- lib/src/splash_screen/splash_screen.dart
+++ lib/src/splash_screen/splash_screen.dart
@@ -18,9 +18,15 @@ class _SplashScreenState extends State<SplashScreen>
     animationController = AnimationController(
       vsync: this,
 
-      duration: Duration(seconds: 5),
+      duration: Duration(seconds: 10),
+
       // reverseDuration: Duration(milliseconds: 800),
     )..repeat();
+    waitAndgo();
+  }
+
+  void waitAndgo() async {
+    await Future.delayed(Duration(seconds: 3));
   }
 
   @override
@@ -29,29 +35,67 @@ class _SplashScreenState extends State<SplashScreen>
     // final arg = Matrix4.columns(
 
     // ;
+    Matrix4 buildRollingMatrix(double angle) {
+      return Matrix4.identity()
+        ..setEntry(3, 2, 0.002) // perspective
+        ..rotateZ(angle); // rotate like a wheel
+    }
+
     return Scaffold(
-      // backgroundColor: Colrs.black,
-      body: Center(
-        child: AnimatedBuilder(
-          animation: animate,
-          builder: (context, child) {
-            final angle = animationController.value * 2 * pi;
+      backgroundColor: Colors.black,
+      body: GestureDetector(
+        onTap: () {},
+        child: Center(
+          child: AnimatedBuilder(
+            animation: animate,
+            builder: (context, child) {
+              final angle = animationController.value * 2 * pi;
 
-            return Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()
-                ..setEntry(3, 2, 0.001)
-                ..rotateZ(angle),
-              // ..rotateX(angle / 3)
-              // ..rotateY(angle / 5),
-              // flipX: true,
-              origin: Offset(0, 0),
+              return Transform(
+                alignment: Alignment.center,
+                transform: buildRollingMatrix(angle),
+                // ..rotateX(angle / 3)
+                // ..rotateY(angle / 5),
+                // flipX: true,
+                origin: Offset(0, 4),
 
-              child: child,
-            );
-          },
-          child: Image.asset("image/welcome.png"),
-          // child: Container(height: 100, width: 100, color: Colors.amber),
+                child: child,
+              );
+            },
+
+            child: MouseRegion(
+              onEnter: (event) {
+                return setState(() {
+                  animationController.stop();
+                });
+              },
+              onExit: (event) {
+                setState(() {
+                  animationController.repeat();
+                });
+              },
+              child: Container(
+                height: 400,
+                width: 400,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage("image/welcome.png"),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      blurRadius: 8,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+
+                // child: ,
+              ),
+            ),
+            // child: Container(height: 100, width: 100, color: Colors.amber),
+          ),
         ),
       ),
     );
