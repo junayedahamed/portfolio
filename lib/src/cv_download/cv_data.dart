53d59e17077d64b512c60670fa8ee998f188d0b2
junayedahamed <junayedahamed660@gmail.com>
2025-10-08 23:38:22 +0600

    cv data updated

--- lib/src/cv_download/cv_data.dart
+++ lib/src/cv_download/cv_data.dart
@@ -2,5 +2,5 @@ class CvData {
   CvData._();
 
   static final cvpath =
-      "https://c9yois02lm.ufs.sh/f/RXJBrPvyVfAx4qY0wVmtSEGbOBR9kznTFfIclVm6tNCwJquA";
+      "https://c9yois02lm.ufs.sh/f/RXJBrPvyVfAxAZd5xuXj4UqhValm1H7pMoFEsJTxD6i85AnP";
 }
